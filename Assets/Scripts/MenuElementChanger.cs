74a851f532de8c1cf29c95489ec3ea322553fcd9
recursivecurse <adityakumarm.cs18@rvce.edu.in>
2022-01-20 09:37:22 +0530

    added files

---
+++ Assets/Scripts/MenuElementChanger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class MenuElementChanger : MonoBehaviour
+{
+	public bool ignoreTransition = false;
+	public MenuElementChanger[] onActivateItems;
+	public UnityEvent onEnable;
+	public UnityEvent onDisable;
+
+	void Start ()
+	{
+		if (ignoreTransition)
+			return;
+		EventTrigger eventTrigger = GetComponent<EventTrigger> ();
+		if (eventTrigger == null)
+			eventTrigger = gameObject.AddComponent<EventTrigger> ();
+		EventTrigger.Entry pointerClick = new EventTrigger.Entry ();
+		pointerClick.eventID = EventTriggerType.PointerClick;
+		pointerClick.callback.AddListener ((eventData) => {
+			GameObject.Find ("System").GetComponent<TransitionManagerMenu> ()
+				.changePosition (GetComponent<MenuElementChanger> ());
+			onDisable.Invoke ();
+			foreach (MenuElementChanger item in onActivateItems) {
+				item.enable ();
+			}
+		});
+		eventTrigger.triggers.Add (pointerClick);
+	}
+
+	public void enable ()
+	{
+		onEnable.Invoke ();
+	}
+
+	public void disable ()
+	{
+		onDisable.Invoke ();
+	}
+}
---
+++ Assets/Scripts/TransitionManagerMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TransitionManagerMenu : MonoBehaviour {
+	public MenuElementChanger lastPosition;
+
+	public Animator hideEye;
+
+	public void changePosition(MenuElementChanger currentItem){
+		if (lastPosition != null) {
+			foreach (MenuElementChanger item in lastPosition.onActivateItems) {
+				item.disable ();
+			}
+		}
+		lastPosition = currentItem;
+	}
+
+	public void LoadScenes(int levelNum)
+	{
+		hideEye.Play("LoadLevel");
+		SceneManager.LoadSceneAsync (levelNum);
+	}
+}
